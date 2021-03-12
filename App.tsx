a0919f0ff0c120a5705f552c5d74bbeeac2e4daf
KelvinMitaki <mitakikelvin1@gmail.com>
2021-03-12 13:28:32 +0300

    wired up react navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./screens/HomeScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const stackNavigator = createStackNavigator({ Home: HomeScreen });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = createAppContainer(stackNavigator);
+
+export default () => <App />;
