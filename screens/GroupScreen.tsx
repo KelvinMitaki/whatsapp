2ff8195670adf96a8c15e4dd56af7c4599f5a11b
KelvinMitaki <mitakikelvin1@gmail.com>
2021-03-13 20:31:22 +0300

    scaffolded grp screen

--- screens/GroupScreen.tsx
+++ screens/GroupScreen.tsx
@@ -1,14 +1,82 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
+import { Text } from "react-native-elements";
+import { Card, Avatar } from "react-native-elements";
+import {
+  TouchableNativeFeedback,
+  TouchableOpacity
+} from "react-native-gesture-handler";
+import { NavigationStackScreenComponent } from "react-navigation-stack";
+import { NavigationMaterialTabScreenComponent } from "react-navigation-tabs";
+import { users } from "../data";
+import inspect from "../inspect";
 
-const GroupScreen = () => {
+const GroupScreen: NavigationMaterialTabScreenComponent = ({ navigation }) => {
   return (
-    <View>
-      <Text>GroupScreen GroupScreen</Text>
+    <View style={styles.prt}>
+      <FlatList
+        data={users}
+        keyExtractor={(_, i) => i.toLocaleString()}
+        renderItem={i => (
+          <TouchableNativeFeedback
+            //@ts-ignore
+            background={TouchableNativeFeedback.Ripple("#FFFFFF", false)}
+            onPress={() => navigation.navigate("Chat")}
+          >
+            <View style={styles.contact}>
+              <Avatar
+                rounded
+                source={require("../assets/blank.png")}
+                size={55}
+              />
+              <View style={styles.contactTxt}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between"
+                  }}
+                >
+                  <Text
+                    style={{ fontSize: 22, fontWeight: "400", color: "#fff" }}
+                  >
+                    Group {i.index + 1}
+                  </Text>
+                  <Text style={{ right: 1, color: "rgba(255,255,255,.6)" }}>
+                    Yesterday
+                  </Text>
+                </View>
+                <Text style={{ marginTop: 5, color: "rgba(255,255,255,.6)" }}>
+                  hello
+                </Text>
+                <Card.Divider
+                  style={{
+                    marginTop: 15,
+                    backgroundColor: "rgba(255,255,255,.3)"
+                  }}
+                />
+              </View>
+            </View>
+          </TouchableNativeFeedback>
+        )}
+      />
     </View>
   );
 };
 
 export default GroupScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  prt: {},
+  contact: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: 70,
+    paddingHorizontal: 10
+  },
+  contactTxt: {
+    paddingLeft: 10,
+    width: "85%",
+    justifyContent: "space-between",
+    height: "100%"
+  }
+});
