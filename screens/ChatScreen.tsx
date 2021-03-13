347cdc08abd18af27272fd84e33c01fe2bdc85de
KelvinMitaki <mitakikelvin1@gmail.com>
2021-03-13 23:43:42 +0300

    added elipsis to chat header

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
-import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+import { MaterialIcons, Ionicons, FontAwesome } from "@expo/vector-icons";
 import { Avatar } from "react-native-elements/dist/avatar/Avatar";
 import inspect from "../inspect";
 import { TouchableNativeFeedback } from "react-native-gesture-handler";
@@ -21,19 +21,27 @@ ChatScreen.navigationOptions = ({}) => {
   return {
     headerTitle: () => (
       <View style={styles.headerLeft}>
-        <Avatar
+        {/* <Avatar
           rounded
           source={require("../assets/blank.png")}
           containerStyle={{ marginLeft: "-8%" }}
           size={40}
-        />
+        /> */}
+        <View style={styles.group}>
+          <FontAwesome
+            name="group"
+            size={25}
+            color="rgba(241, 241, 242, 0.8)"
+          />
+        </View>
         <Text
           numberOfLines={1}
           style={{
             color: "white",
             marginLeft: 10,
             fontSize: 20,
-            fontWeight: "400"
+            fontWeight: "400",
+            width: "75%"
           }}
         >
           Kevin
@@ -84,11 +92,20 @@ export default ChatScreen;
 const styles = StyleSheet.create({
   headerLeft: {
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    marginLeft: "-10%"
   },
   headerRight: {
     flexDirection: "row",
     width: 100,
     justifyContent: "space-around"
+  },
+  group: {
+    borderRadius: 70,
+    backgroundColor: "grey",
+    height: 40,
+    width: 40,
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
