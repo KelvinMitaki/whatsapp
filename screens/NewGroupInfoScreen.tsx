b2db6a1a8f79b5290609c9348e46aee014f0c42a
KelvinMitaki <mitakikelvin1@gmail.com>
2021-03-19 01:03:16 +0300

    wired up second section of NewGroupInfoScreen

--- screens/NewGroupInfoScreen.tsx
+++ screens/NewGroupInfoScreen.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import { StyleSheet, TouchableNativeFeedback, View } from "react-native";
+import {
+  Dimensions,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  TouchableNativeFeedback,
+  View
+} from "react-native";
 import { Input, Text } from "react-native-elements";
 import { FontAwesome, Fontisto, Ionicons } from "@expo/vector-icons";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 import inspect from "../inspect";
 
 const NewGroupInfoScreen: NavigationStackScreenComponent = ({}) => {
+  const contacts = [];
+  for (let i = 0; i < 100; i++) {
+    contacts.push(i);
+  }
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <View
         style={{
           height: 125,
@@ -58,7 +69,43 @@ const NewGroupInfoScreen: NavigationStackScreenComponent = ({}) => {
           </TouchableNativeFeedback>
         </View>
       </View>
-      <View></View>
+      <View style={{ flex: 1 }}>
+        <Text style={{ color: "rgba(255,255,255,.5)", margin: 20 }}>
+          Participants: 25
+        </Text>
+        <View style={{ flex: 1, alignItems: "center" }}>
+          <FlatList
+            data={contacts}
+            keyExtractor={(_, i) => i.toLocaleString()}
+            numColumns={Math.floor(Dimensions.get("screen").width / 60)}
+            renderItem={() => (
+              <View
+                style={{
+                  width: 60,
+                  height: 70
+                }}
+              >
+                <View style={styles.person}>
+                  <Ionicons
+                    name="person"
+                    size={35}
+                    color="rgba(241, 241, 242, 0.8)"
+                  />
+                </View>
+                <Text
+                  numberOfLines={1}
+                  style={{ color: "rgba(255,255,255,.8)" }}
+                >
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Deleniti, voluptas saepe tempora tenetur accusamus culpa, est
+                  soluta pariatur expedita a officiis corrupti doloribus numquam
+                  temporibus dignissimos consectetur amet nulla maiores?
+                </Text>
+              </View>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -115,5 +162,13 @@ const styles = StyleSheet.create({
     width: 50,
     alignItems: "center",
     justifyContent: "center"
+  },
+  person: {
+    height: 45,
+    width: 45,
+    borderRadius: 500,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "grey"
   }
 });
