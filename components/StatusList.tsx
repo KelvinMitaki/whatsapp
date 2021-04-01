c0d2c9c55d3cb97cb1ebe32e69582c0a29c87252
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-01 08:59:24 +0300

    fixed scroll issue in status

--- components/StatusList.tsx
+++ components/StatusList.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
-import { ScrollView, TouchableNativeFeedback } from "react-native";
-import {
-  Ionicons,
-  FontAwesome,
-  MaterialCommunityIcons
-} from "@expo/vector-icons";
+import { TouchableNativeFeedback } from "react-native";
 import inspect from "../inspect";
-import { NavigationMaterialTabScreenComponent } from "react-navigation-tabs";
 import { DashedCircularIndicator } from "../components/DashedCircularIndicator";
-import StatusList from "../components/StatusList";
+import { NavigationInjectedProps, withNavigation } from "react-navigation";
 
-const StatusScreen: NavigationMaterialTabScreenComponent = ({ navigation }) => {
+const StatusList: React.FC<NavigationInjectedProps> = ({ navigation }) => {
   const status = [];
 
   for (let i = 0; i < 50; i++) {
@@ -19,8 +13,42 @@ const StatusScreen: NavigationMaterialTabScreenComponent = ({ navigation }) => {
   }
   return (
     <View>
-      <ScrollView>
+      <Text style={styles.title}>Recent Updates</Text>
+      {status.map((_, i) => (
         <TouchableNativeFeedback
+          key={i}
+          onPress={() => {}}
+          background={TouchableNativeFeedback.Ripple("#fff", false)}
+        >
+          <View style={styles.statusPrt}>
+            <DashedCircularIndicator
+              strokeWidth={2}
+              radius={32.5}
+              label={
+                <Image
+                  source={require("../assets/1.jpg")}
+                  style={styles.statusImg}
+                />
+              }
+              backgroundColor="#191f23"
+              selectedValue={6}
+              activeStrokeColor="#00af9c"
+            />
+            <View style={styles.statusMetaData}>
+              <Text style={{ color: "white", fontSize: 18 }} numberOfLines={1}>
+                Kevin
+              </Text>
+              <Text style={{ color: "rgba(255,255,255,.5)" }}>
+                Today, 7:44 PM
+              </Text>
+            </View>
+          </View>
+        </TouchableNativeFeedback>
+      ))}
+      <Text style={styles.title}>Viewed Updates</Text>
+      {status.map((_, i) => (
+        <TouchableNativeFeedback
+          key={i}
           onPress={() => {}}
           background={TouchableNativeFeedback.Ripple("#fff", false)}
         >
@@ -37,44 +65,21 @@ const StatusScreen: NavigationMaterialTabScreenComponent = ({ navigation }) => {
               backgroundColor="#191f23"
             />
             <View style={styles.statusMetaData}>
-              <Text style={{ color: "white", fontSize: 18 }}>My Status</Text>
+              <Text style={{ color: "white", fontSize: 18 }} numberOfLines={1}>
+                Kevin
+              </Text>
               <Text style={{ color: "rgba(255,255,255,.5)" }}>
                 Today, 7:44 PM
               </Text>
             </View>
-            <View style={{ marginLeft: "-12%" }}>
-              <TouchableNativeFeedback
-                background={TouchableNativeFeedback.Ripple("#fff", true)}
-                onPress={() => navigation.navigate("MyStatus")}
-              >
-                <View style={styles.ellipsis}>
-                  <Ionicons
-                    name="ellipsis-horizontal-sharp"
-                    size={20}
-                    color="rgba(255,255,255,.5)"
-                  />
-                </View>
-              </TouchableNativeFeedback>
-            </View>
           </View>
         </TouchableNativeFeedback>
-        <StatusList />
-      </ScrollView>
-      <TouchableNativeFeedback onPress={() => {}}>
-        <View style={styles.pencil}>
-          <MaterialCommunityIcons name="pencil" size={20} color="#fff" />
-        </View>
-      </TouchableNativeFeedback>
-      <TouchableNativeFeedback onPress={() => {}}>
-        <View style={styles.camera}>
-          <FontAwesome name="camera" size={20} color="#fff" />
-        </View>
-      </TouchableNativeFeedback>
+      ))}
     </View>
   );
 };
 
-export default StatusScreen;
+export default withNavigation(StatusList);
 
 const styles = StyleSheet.create({
   statusPrt: {
