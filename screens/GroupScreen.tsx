40e537280fac7119d8979c9b1c68ac2d5e54c0e3
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-01 08:35:59 +0300

    fixed scroll issue in group

--- screens/GroupScreen.tsx
+++ screens/GroupScreen.tsx
@@ -7,84 +7,12 @@ import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import { NavigationMaterialTabScreenComponent } from "react-navigation-tabs";
 import { users } from "../data";
 import inspect from "../inspect";
+import GroupChat from "../components/GroupChat";
 
 const GroupScreen: NavigationMaterialTabScreenComponent = ({ navigation }) => {
   return (
     <View style={styles.prt}>
-      <FlatList
-        data={users}
-        keyExtractor={(_, i) => i.toLocaleString()}
-        renderItem={i => (
-          <TouchableNativeFeedback
-            background={TouchableNativeFeedback.Ripple("#FFFFFF", false)}
-            onPress={() => navigation.navigate("GroupChat")}
-          >
-            <View style={styles.contact}>
-              {/* <Avatar
-                rounded
-                source={require("../assets/blank.png")}
-                size={55}
-              /> */}
-              <View style={styles.group}>
-                <FontAwesome
-                  name="group"
-                  size={35}
-                  color="rgba(241, 241, 242, 0.8)"
-                />
-              </View>
-              <View style={styles.contactTxt}>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    justifyContent: "space-between"
-                  }}
-                >
-                  <Text
-                    style={{
-                      fontSize: 22,
-                      fontWeight: "400",
-                      color: "#fff",
-                      width: "78%"
-                    }}
-                    numberOfLines={1}
-                  >
-                    Group {i.index + 1}
-                  </Text>
-                  <Text style={{ right: 1, color: "rgba(255,255,255,.6)" }}>
-                    Yesterday
-                  </Text>
-                </View>
-                <View style={styles.msg}>
-                  <Text
-                    numberOfLines={1}
-                    style={{
-                      marginTop: 5,
-                      color: "rgba(255,255,255,.6)",
-                      width: "90%"
-                    }}
-                  >
-                    +2547 2155 9392: hello Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit. Commodi sed et vel fuga
-                    quibusdam nihil dolorum temporibus, veniam enim error? Saepe
-                    in vel enim, repellat dicta distinctio porro molestias
-                    alias?
-                  </Text>
-                  <Badge
-                    value="99"
-                    badgeStyle={{ backgroundColor: "#00af9c" }}
-                  />
-                </View>
-                <Card.Divider
-                  style={{
-                    marginTop: 15,
-                    backgroundColor: "rgba(255,255,255,.3)"
-                  }}
-                />
-              </View>
-            </View>
-          </TouchableNativeFeedback>
-        )}
-      />
+      <GroupChat />
       <TouchableNativeFeedback onPress={() => navigation.navigate("Contact")}>
         <View style={styles.plus}>
           <MaterialIcons name="message" size={25} color="#fff" />
