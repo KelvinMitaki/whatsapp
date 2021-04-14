35e170dc805ba08fc2d3b6ee7b7d786d57493432
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-14 17:02:44 +0300

    wired uo privacy screen

--- screens/PrivacyScreen.tsx
+++ screens/PrivacyScreen.tsx
@@ -1,14 +1,65 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableNativeFeedback, View } from "react-native";
 
 const PrivacyScreen = () => {
   return (
     <View>
-      <Text>PrivacyScreen PrivacyScreen</Text>
+      <View style={styles.privacyItem}>
+        <Text style={styles.PrivacyTitle}>Who can see my personal info</Text>
+        <Text style={{ color: "rgba(241, 241, 242, 0.7)" }}>
+          If you don't share your Last Seen, you won't be able to see other
+          people's Last Seen
+        </Text>
+      </View>
+      <TouchableNativeFeedback
+        background={TouchableNativeFeedback.Ripple("#fff", false)}
+        onPress={() => {}}
+      >
+        <View style={styles.privacyItem}>
+          <Text style={{ color: "#fff" }}>Last Seen</Text>
+          <Text style={{ color: "rgba(241, 241, 242, 0.7)" }}>Everyone</Text>
+        </View>
+      </TouchableNativeFeedback>
+      <TouchableNativeFeedback
+        background={TouchableNativeFeedback.Ripple("#fff", false)}
+        onPress={() => {}}
+      >
+        <View style={styles.privacyItem}>
+          <Text style={{ color: "#fff" }}>Profile Photo</Text>
+          <Text style={{ color: "rgba(241, 241, 242, 0.7)" }}>Everyone</Text>
+        </View>
+      </TouchableNativeFeedback>
+      <TouchableNativeFeedback
+        background={TouchableNativeFeedback.Ripple("#fff", false)}
+        onPress={() => {}}
+      >
+        <View style={styles.privacyItem}>
+          <Text style={{ color: "#fff" }}>My Contacts</Text>
+          <Text style={{ color: "rgba(241, 241, 242, 0.7)" }}>Everyone</Text>
+        </View>
+      </TouchableNativeFeedback>
+      <TouchableNativeFeedback
+        background={TouchableNativeFeedback.Ripple("#fff", false)}
+        onPress={() => {}}
+      >
+        <View style={styles.privacyItem}>
+          <Text style={{ color: "#fff" }}>Blocked Contacts</Text>
+          <Text style={{ color: "rgba(241, 241, 242, 0.7)" }}>None</Text>
+        </View>
+      </TouchableNativeFeedback>
     </View>
   );
 };
 
 export default PrivacyScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  PrivacyTitle: {
+    color: "#00af9c",
+    fontWeight: "bold",
+    marginBottom: 5
+  },
+  privacyItem: {
+    padding: 20
+  }
+});
