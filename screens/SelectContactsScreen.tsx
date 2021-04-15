db934d79fe64345e4819d76eb09c8507daf5a6b8
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-15 17:40:56 +0300

    added checkbox animation in SelectContactsScreen

--- screens/SelectContactsScreen.tsx
+++ screens/SelectContactsScreen.tsx
@@ -1,25 +1,57 @@
-import React from "react";
-import { StyleSheet, TouchableNativeFeedback, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { Animated, StyleSheet, TouchableNativeFeedback, View } from "react-native";
 import { Text } from "react-native-elements";
 import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 
 const SelectContactsScreen: NavigationStackScreenComponent = () => {
+  const [checked, setChecked] = useState<boolean>(false);
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const fadeIn = () => {
+    Animated.timing(fadeAnim, { toValue: 1, useNativeDriver: false, duration: 200 }).start();
+  };
+  const fadeOut = () => {
+    Animated.timing(fadeAnim, { toValue: 0, useNativeDriver: false, duration: 200 }).start();
+  };
+  useEffect(() => {
+    if (checked) {
+      fadeIn();
+    } else {
+      fadeOut();
+    }
+  }, [checked]);
   return (
     <View>
-      <View style={styles.contact}>
-        <View style={styles.person}>
-          <Ionicons name="person" size={30} color="rgba(241, 241, 242, 0.8)" />
-        </View>
-        <View style={styles.info}>
-          <Text numberOfLines={1} style={{ color: "#fff", width: "88%" }}>
-            SelectContactsScreen SelectContactsScreen
-          </Text>
-          <View style={styles.check}>
-            <MaterialIcons name="check" size={18} />
+      <TouchableNativeFeedback
+        background={TouchableNativeFeedback.Ripple("#fff", false)}
+        onPress={() => setChecked(c => !c)}
+      >
+        <View style={styles.contact}>
+          <View style={styles.person}>
+            <Ionicons name="person" size={30} color="rgba(241, 241, 242, 0.8)" />
+          </View>
+          <View style={styles.info}>
+            <Text numberOfLines={1} style={{ color: "#fff", width: "88%" }}>
+              SelectContactsScreen SelectContactsScreen
+            </Text>
+            <View style={styles.info_outline}>
+              <Animated.View
+                style={[
+                  styles.check,
+                  {
+                    opacity: fadeAnim,
+                    transform: [
+                      { scale: fadeAnim.interpolate({ inputRange: [0, 1], outputRange: [0, 1] }) }
+                    ]
+                  }
+                ]}
+              >
+                <MaterialIcons name="check" size={18} color="#191f23" />
+              </Animated.View>
+            </View>
           </View>
         </View>
-      </View>
+      </TouchableNativeFeedback>
     </View>
   );
 };
@@ -102,10 +134,19 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   check: {
-    height: 25,
-    width: 25,
-    borderRadius: 25,
+    height: 20,
+    width: 20,
+    borderRadius: 20,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  info_outline: {
+    height: 20,
+    width: 20,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
   }
