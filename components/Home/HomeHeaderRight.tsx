f61168b61945a4d2678763466f8f42db115b8ee4
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-18 17:49:32 +0300

    animated custom modal in HomeHeaderRight

--- components/Home/HomeHeaderRight.tsx
+++ components/Home/HomeHeaderRight.tsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from "react";
-import { StyleSheet, Text, TouchableNativeFeedback, View } from "react-native";
+import { StyleSheet, Text, TouchableNativeFeedback, View, Animated } from "react-native";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import CustomModal from "../Modals/CustomModal";
 
 const HomeHeaderRight = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
-
+  const height = useRef(new Animated.Value(0)).current;
+  const width = useRef(new Animated.Value(0)).current;
   return (
     <View style={styles.headerRight}>
       <View style={styles.ellipsis}>
@@ -21,14 +22,33 @@ const HomeHeaderRight = () => {
       <View style={styles.ellipsis}>
         <TouchableNativeFeedback
           background={TouchableNativeFeedback.Ripple("#fff", true)}
-          onPress={() => setShowModal(true)}
+          onPress={() => {
+            setShowModal(true);
+            Animated.parallel([
+              Animated.timing(height, {
+                toValue: 200,
+                useNativeDriver: false,
+                duration: 300
+              }),
+              Animated.timing(width, {
+                toValue: 150,
+                useNativeDriver: false,
+                duration: 300
+              })
+            ]).start();
+          }}
         >
           <View>
             <Ionicons name="ellipsis-vertical-sharp" size={20} color="#fff" />
           </View>
         </TouchableNativeFeedback>
       </View>
-      <CustomModal showModal={showModal} setShowModal={setShowModal} />
+      <CustomModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        height={height}
+        width={width}
+      />
     </View>
   );
 };
--- components/Modals/CustomModal.tsx
+++ components/Modals/CustomModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import {
   Modal,
   StyleSheet,
@@ -6,7 +6,8 @@ import {
   View,
   TouchableNativeFeedback,
   TouchableOpacity,
-  TouchableWithoutFeedback
+  TouchableWithoutFeedback,
+  Animated
 } from "react-native";
 import { NavigationInjectedProps, withNavigation } from "react-navigation";
 import inspect from "../../inspect";
@@ -14,39 +15,59 @@ import inspect from "../../inspect";
 interface Props {
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
   showModal: boolean;
+  height: Animated.Value;
+  width: Animated.Value;
 }
 
 const CustomModal: React.FC<Props & NavigationInjectedProps> = ({
   setShowModal,
   showModal,
-  navigation
+  navigation,
+  height,
+  width
 }) => {
+  const reset = () => {
+    Animated.parallel([
+      Animated.timing(height, {
+        toValue: 200,
+        useNativeDriver: false,
+        duration: 1000
+      }),
+      Animated.timing(width, {
+        toValue: 150,
+        useNativeDriver: false,
+        duration: 1000
+      })
+    ]).reset();
+  };
   return (
-    <Modal
-      transparent
-      onRequestClose={() => setShowModal(false)}
-      visible={showModal}
-      animationType="fade"
-    >
+    <Modal transparent onRequestClose={() => setShowModal(false)} visible={showModal}>
       <TouchableWithoutFeedback
         accessible={false}
-        onPress={() => setShowModal(false)}
+        onPress={() => {
+          setShowModal(false);
+          reset();
+        }}
         touchSoundDisabled
       >
         <View style={{ height: "100%", width: "100%" }}>
-          <View
-            style={{
-              backgroundColor: "#14191d",
-              width: 150,
-              alignSelf: "flex-end",
-              marginRight: 5,
-              marginTop: 5
-            }}
+          <Animated.View
+            style={[
+              {
+                backgroundColor: "#14191d",
+                alignSelf: "flex-end",
+                marginRight: 5,
+                marginTop: 5,
+                overflow: "hidden"
+              },
+              { height, width }
+            ]}
           >
             <TouchableNativeFeedback
               onPress={() => {
                 setShowModal(false);
                 navigation.navigate("NewGroup");
+                reset();
               }}
               background={TouchableNativeFeedback.Ripple("#FFFFFF", false)}
             >
@@ -66,6 +87,7 @@ const CustomModal: React.FC<Props & NavigationInjectedProps> = ({
               onPress={() => {
                 setShowModal(false);
                 navigation.navigate("Broadcast");
+                reset();
               }}
               background={TouchableNativeFeedback.Ripple("#FFFFFF", false)}
             >
@@ -85,6 +107,7 @@ const CustomModal: React.FC<Props & NavigationInjectedProps> = ({
               onPress={() => {
                 setShowModal(false);
                 navigation.navigate("StarredMessages");
+                reset();
               }}
               background={TouchableNativeFeedback.Ripple("#FFFFFF", false)}
             >
@@ -104,6 +127,7 @@ const CustomModal: React.FC<Props & NavigationInjectedProps> = ({
               onPress={() => {
                 setShowModal(false);
                 navigation.navigate("Settings");
+                reset();
               }}
               background={TouchableNativeFeedback.Ripple("#FFFFFF", false)}
             >
@@ -119,7 +143,7 @@ const CustomModal: React.FC<Props & NavigationInjectedProps> = ({
                 </Text>
               </View>
             </TouchableNativeFeedback>
-          </View>
+          </Animated.View>
         </View>
       </TouchableWithoutFeedback>
     </Modal>
