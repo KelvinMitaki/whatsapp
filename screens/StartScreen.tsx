3b2dd6c23d495eff9bb6a7b4ac96cefbc7c11b0c
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-22 13:57:50 +0300

    wired up startscreen ui

--- screens/StartScreen.tsx
+++ screens/StartScreen.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Button } from "react-native-elements";
 
 const StartScreen = () => {
   return (
     <View>
-      <Text style={{ color: "#fff" }}>StartScreen StartScreen</Text>
+      <Text style={{ color: "#fff", marginTop: "20%", fontSize: 30, alignSelf: "center" }}>
+        Welcome to ChatApp
+      </Text>
+      <Image
+        source={require("../assets/hero.png")}
+        style={{ width: "70%", alignSelf: "center" }}
+        resizeMode="contain"
+      />
+      <Button
+        title="AGREE AND CONTINUE"
+        containerStyle={{ alignSelf: "center", width: "70%" }}
+        buttonStyle={{ backgroundColor: "#00af9c" }}
+        titleStyle={{ color: "#191f23" }}
+      />
     </View>
   );
 };
