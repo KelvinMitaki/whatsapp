4664b0239fd9fea12becb54ac50d7431bd709d6d
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-30 19:53:12 +0300

    wired up fetchCurrentUser on blankscreen

--- screens/BlankScreen.tsx
+++ screens/BlankScreen.tsx
@@ -1,20 +1,30 @@
+import { useLazyQuery } from "@apollo/client";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useEffect } from "react";
 import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
+import { FETCH_CURRENT_USER } from "../graphql/queries";
 
 const BlankScreen: NavigationStackScreenComponent = ({ navigation }) => {
+  const [fetchCurrentUser] = useLazyQuery(FETCH_CURRENT_USER, {
+    onCompleted() {
+      navigation.replace("Home");
+    },
+    onError() {
+      navigation.replace("Start");
+    }
+  });
   useEffect(() => {
-    const loginUser = async () => {
-      const token = await AsyncStorage.getItem("token");
-      if (!token) {
-        navigation.replace("Start");
-      } else {
-        navigation.replace("Home");
-      }
-    };
     loginUser();
   }, []);
+  const loginUser = async () => {
+    const token = await AsyncStorage.getItem("token");
+    if (!token) {
+      navigation.replace("Start");
+    } else {
+      fetchCurrentUser();
+    }
+  };
   return (
     <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
       <ActivityIndicator color="#00af9c" size="large" />
