b7c5f3d144dbca0a454722d55b01d4bef7c3cf86
KelvinMitaki <mitakikelvin1@gmail.com>
2021-04-30 23:56:29 +0300

    wired up displaying fetched chats

--- components/Home/HomeChat.tsx
+++ components/Home/HomeChat.tsx
@@ -10,9 +10,12 @@ import { Redux } from "../../interfaces/Redux";
 import inspect from "../../inspect";
 import { SetSearchModal } from "../../screens/HomeScreen";
 import { MessageMeta } from "../../data/messages";
+import { FETCH_CHATS } from "../../graphql/queries";
+import { useQuery } from "@apollo/client";
 
 const HomeChat: React.FC<NavigationInjectedProps> = ({ navigation }) => {
   const messages = useSelector((state: Redux) => state.chat.messages);
+  const { data } = useQuery(FETCH_CHATS, { fetchPolicy: "cache-only" });
   const dispatch = useDispatch();
   const renderItem = ({
     item: { message, messageNumber, type, time, name }
@@ -103,34 +106,41 @@ const HomeChat: React.FC<NavigationInjectedProps> = ({ navigation }) => {
   const keyExtractor = (_: MessageMeta, i: number) => i.toLocaleString();
   return (
     <View style={styles.prt}>
-      <FlatList
-        data={[]}
-        keyExtractor={keyExtractor}
-        renderItem={renderItem}
-        getItemLayout={getItemLayout}
-        onTouchStart={() => dispatch<SetSearchModal>({ type: "setSearchModal", payload: false })}
-      />
-      <View style={styles.noMsgs}>
-        <Text
-          style={{ color: "rgba(255,255,255,.7)", textAlign: "center", justifyContent: "center" }}
-        >
-          To start messaging contacts who have ChatApp, tap{" "}
-        </Text>
-        <View style={{ flexDirection: "row" }}>
-          <MaterialCommunityIcons
-            name="android-messages"
-            size={20}
-            color="rgba(255,255,255,.7)"
-            style={styles.msgIcon}
-          />
+      {data && data.fetchChats && data.fetchChats.length ? (
+        <FlatList
+          data={data.fetchChats}
+          keyExtractor={keyExtractor}
+          renderItem={renderItem}
+          getItemLayout={getItemLayout}
+          onTouchStart={() => dispatch<SetSearchModal>({ type: "setSearchModal", payload: false })}
+        />
+      ) : (
+        <View style={styles.noMsgs}>
           <Text
             style={{ color: "rgba(255,255,255,.7)", textAlign: "center", justifyContent: "center" }}
           >
-            {" "}
-            at the bottom right of your screen.
+            To start messaging contacts who have ChatApp, tap{" "}
           </Text>
+          <View style={{ flexDirection: "row" }}>
+            <MaterialCommunityIcons
+              name="android-messages"
+              size={20}
+              color="rgba(255,255,255,.7)"
+              style={styles.msgIcon}
+            />
+            <Text
+              style={{
+                color: "rgba(255,255,255,.7)",
+                textAlign: "center",
+                justifyContent: "center"
+              }}
+            >
+              {" "}
+              at the bottom right of your screen.
+            </Text>
+          </View>
         </View>
-      </View>
+      )}
     </View>
   );
 };
