ac86afc79f317c79e214251d965fac2ce6993b8c
KelvinMitaki <mitakikelvin1@gmail.com>
2021-06-13 20:04:02 +0300

    added useFetchCurrentUserQuery in Message tsx

--- components/Chat/Message.tsx
+++ components/Chat/Message.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from 'react'
 import {
   StyleSheet,
   Text,
@@ -6,43 +6,44 @@ import {
   ScrollView,
   NativeScrollEvent,
   ActivityIndicator,
-  TouchableNativeFeedback
-} from "react-native";
-import { Ionicons, Entypo } from "@expo/vector-icons";
-import inspect from "../../inspect";
+  TouchableNativeFeedback,
+} from 'react-native'
+import { Ionicons, Entypo } from '@expo/vector-icons'
+import inspect from '../../inspect'
 import {
   Chat,
   CurrentUser,
   MessageCountInterface,
-  MessageInterface
-} from "../../interfaces/ChatInterface";
-import { LazyQueryResult, OperationVariables, useQuery, useSubscription } from "@apollo/client";
-import { FETCH_CHATS, FETCH_CURRENT_USER, FETCH_MESSAGES_COUNT } from "../../graphql/queries";
-import format from "date-fns/format";
-import { ADD_NEW_MESSAGE_SUB } from "../../graphql/subscriptions";
-import AppColors from "../../Colors/color";
-import { MESSAGE_LIMIT } from "./Input";
-import { useDispatch, useSelector } from "react-redux";
-import { Redux } from "../../interfaces/Redux";
+  MessageInterface,
+} from '../../interfaces/ChatInterface'
+import { LazyQueryResult, OperationVariables, useQuery, useSubscription } from '@apollo/client'
+import { FETCH_CHATS, FETCH_CURRENT_USER, FETCH_MESSAGES_COUNT } from '../../graphql/queries'
+import format from 'date-fns/format'
+import { ADD_NEW_MESSAGE_SUB } from '../../graphql/subscriptions'
+import AppColors from '../../Colors/color'
+import { MESSAGE_LIMIT } from './Input'
+import { useDispatch, useSelector } from 'react-redux'
+import { Redux } from '../../interfaces/Redux'
+import { useFetchCurrentUserQuery } from '../../generated/graphql'
 
 interface Props {
-  messages: MessageInterface[];
-  recipient: string;
-  fetchMore: LazyQueryResult<any, OperationVariables>["fetchMore"] | undefined;
-  setShowLoading: React.Dispatch<React.SetStateAction<boolean>>;
-  showLoading: boolean;
-  keyboardShown: boolean;
-  setSelectedMsgs: React.Dispatch<React.SetStateAction<MessageInterface[]>>;
-  selectedMsgs: MessageInterface[];
-  chatID: string;
+  messages: MessageInterface[]
+  recipient: string
+  fetchMore: LazyQueryResult<any, OperationVariables>['fetchMore'] | undefined
+  setShowLoading: React.Dispatch<React.SetStateAction<boolean>>
+  showLoading: boolean
+  keyboardShown: boolean
+  setSelectedMsgs: React.Dispatch<React.SetStateAction<MessageInterface[]>>
+  selectedMsgs: MessageInterface[]
+  chatID: string
 }
 
 export interface SetShouldScrollToBottomOnNewMessages {
-  type: "setShouldScrollToBottomOnNewMessages";
-  payload: boolean;
+  type: 'setShouldScrollToBottomOnNewMessages'
+  payload: boolean
 }
 
-const Message: React.FC<Props> = props => {
+const Message: React.FC<Props> = (props) => {
   const {
     messages,
     recipient,
@@ -52,54 +53,54 @@ const Message: React.FC<Props> = props => {
     keyboardShown,
     selectedMsgs,
     setSelectedMsgs,
-    chatID
-  } = props;
-  const { data } = useQuery(FETCH_CURRENT_USER);
-  const currentUser: CurrentUser = data.fetchCurrentUser;
-  const { data: chatsData } = useQuery(FETCH_CHATS);
+    chatID,
+  } = props
+  const { data } = useFetchCurrentUserQuery()
+  const currentUser = data?.fetchCurrentUser
+  const { data: chatsData } = useQuery(FETCH_CHATS)
   const count = useQuery(FETCH_MESSAGES_COUNT, {
     variables: {
-      userIDs: (chatsData.fetchChats as Chat[]).map(c =>
-        c.sender._id === currentUser._id ? c.recipient._id : c.sender._id
-      )
-    }
-  });
+      userIDs: (chatsData.fetchChats as Chat[]).map((c) =>
+        c.sender._id === (currentUser && currentUser._id) ? c.recipient._id : c.sender._id
+      ),
+    },
+  })
   const shouldScrollToBottomOnNewMessages = useSelector(
     (state: Redux) => state.chat.shouldScrollToBottomOnNewMessages
-  );
-  const dispatch = useDispatch();
-  const [subScriptionMsgs, setSubScriptionMsgs] = useState<MessageInterface[]>([]);
+  )
+  const dispatch = useDispatch()
+  const [subScriptionMsgs, setSubScriptionMsgs] = useState<MessageInterface[]>([])
   useSubscription(ADD_NEW_MESSAGE_SUB, {
-    variables: { sender: currentUser._id, recipient },
+    variables: { sender: currentUser?._id, recipient },
     onSubscriptionData(data) {
       if (!shouldScrollToBottomOnNewMessages) {
         dispatch<SetShouldScrollToBottomOnNewMessages>({
-          type: "setShouldScrollToBottomOnNewMessages",
-          payload: true
-        });
+          type: 'setShouldScrollToBottomOnNewMessages',
+          payload: true,
+        })
       }
-      setSubScriptionMsgs(m => [...m, data.subscriptionData.data.addNewMessage]);
-    }
-  });
-  const scrollViewRef = useRef<ScrollView>(null);
+      setSubScriptionMsgs((m) => [...m, data.subscriptionData.data.addNewMessage])
+    },
+  })
+  const scrollViewRef = useRef<ScrollView>(null)
   useEffect(() => {
     if (scrollViewRef.current && showLoading && shouldScrollToBottomOnNewMessages) {
-      scrollViewRef.current.scrollToEnd();
+      scrollViewRef.current.scrollToEnd()
     }
-  }, [messages, subScriptionMsgs, keyboardShown]);
+  }, [messages, subScriptionMsgs, keyboardShown])
   const isCloseToTop = ({ contentOffset }: NativeScrollEvent) => {
-    return contentOffset.y === 0;
-  };
+    return contentOffset.y === 0
+  }
   const filteredMsgs = [...messages, ...subScriptionMsgs]
-    .filter((m, i, s) => i === s.findIndex(ms => ms._id === m._id))
+    .filter((m, i, s) => i === s.findIndex((ms) => ms._id === m._id))
     .filter(
-      msg =>
-        (msg.sender === currentUser._id && msg.recipient === recipient) ||
-        (msg.sender === recipient && msg.recipient === currentUser._id)
+      (msg) =>
+        (msg.sender === currentUser?._id && msg.recipient === recipient) ||
+        (msg.sender === recipient && msg.recipient === currentUser?._id)
     )
-    .sort((a, b) => parseInt(a.createdAt) - parseInt(b.createdAt));
+    .sort((a, b) => parseInt(a.createdAt) - parseInt(b.createdAt))
   return (
-    <View style={{ height: keyboardShown ? "85%" : "90%" }}>
+    <View style={{ height: keyboardShown ? '85%' : '90%' }}>
       <ScrollView
         ref={scrollViewRef}
         onScroll={({ nativeEvent }) => {
@@ -108,34 +109,34 @@ const Message: React.FC<Props> = props => {
             fetchMore &&
             count.data &&
             ((count.data.fetchMessagesCount as MessageCountInterface[]).find(
-              mc => mc.chatID === chatID
+              (mc) => mc.chatID === chatID
             )?.messageCount || 0) > filteredMsgs.length
           ) {
             if (!shouldScrollToBottomOnNewMessages) {
               dispatch<SetShouldScrollToBottomOnNewMessages>({
-                type: "setShouldScrollToBottomOnNewMessages",
-                payload: true
-              });
+                type: 'setShouldScrollToBottomOnNewMessages',
+                payload: true,
+              })
             }
-            setShowLoading(false);
-            fetchMore({ variables: { offset: filteredMsgs.length, limit: MESSAGE_LIMIT } });
+            setShowLoading(false)
+            fetchMore({ variables: { offset: filteredMsgs.length, limit: MESSAGE_LIMIT } })
           }
         }}
       >
         {filteredMsgs.map((item, index) => (
           <TouchableNativeFeedback
-            onLongPress={() => !selectedMsgs.length && setSelectedMsgs(msgs => [item, ...msgs])}
+            onLongPress={() => !selectedMsgs.length && setSelectedMsgs((msgs) => [item, ...msgs])}
             onPress={() =>
               selectedMsgs.length &&
-              setSelectedMsgs(msgs => {
-                let selectedMsgs = [...msgs];
-                const selectedMsgIndex = selectedMsgs.findIndex(msg => msg._id === item._id);
+              setSelectedMsgs((msgs) => {
+                let selectedMsgs = [...msgs]
+                const selectedMsgIndex = selectedMsgs.findIndex((msg) => msg._id === item._id)
                 if (selectedMsgIndex !== -1) {
-                  selectedMsgs.splice(selectedMsgIndex, 1);
+                  selectedMsgs.splice(selectedMsgIndex, 1)
                 } else {
-                  selectedMsgs = [item, ...selectedMsgs];
+                  selectedMsgs = [item, ...selectedMsgs]
                 }
-                return selectedMsgs;
+                return selectedMsgs
               })
             }
             key={item._id}
@@ -145,26 +146,26 @@ const Message: React.FC<Props> = props => {
               {index === 0 &&
                 count.data &&
                 ((count.data.fetchMessagesCount as MessageCountInterface[]).find(
-                  mc => mc.chatID === chatID
+                  (mc) => mc.chatID === chatID
                 )?.messageCount || 0) > filteredMsgs.length &&
                 filteredMsgs.length > 20 && (
                   <ActivityIndicator size="large" color={AppColors.secodary} />
                 )}
-              {selectedMsgs.some(msg => msg._id === item._id) && (
+              {selectedMsgs.some((msg) => msg._id === item._id) && (
                 <View style={styles.selectedMsgPrt}>
                   <View style={styles.selectedMsg}>
-                    <Text style={{ color: "transparent" }}>{item.message}</Text>
+                    <Text style={{ color: 'transparent' }}>{item.message}</Text>
                   </View>
                 </View>
               )}
-              {currentUser._id === item.sender ? (
+              {currentUser?._id === item.sender ? (
                 <View style={styles.me}>
-                  <Text style={{ color: "#fff" }}>{item.message}</Text>
+                  <Text style={{ color: '#fff' }}>{item.message}</Text>
                   <Text style={styles.meta}>
-                    {item.starredBy.some(id => id === currentUser._id) && (
+                    {item.starredBy.some((id) => id === currentUser?._id) && (
                       <Entypo name="star" size={13} />
-                    )}{" "}
-                    {format(new Date(parseInt(item.createdAt)), "p")}{" "}
+                    )}{' '}
+                    {format(new Date(parseInt(item.createdAt)), 'p')}{' '}
                     {item.read ? (
                       <Ionicons name="checkmark-done" size={18} color={AppColors.blue_tick} />
                     ) : (
@@ -174,12 +175,12 @@ const Message: React.FC<Props> = props => {
                 </View>
               ) : (
                 <View style={styles.sender}>
-                  <Text style={{ color: "#fff" }}>{item.message}</Text>
+                  <Text style={{ color: '#fff' }}>{item.message}</Text>
                   <Text style={styles.meta}>
-                    {item.starredBy.some(id => id === currentUser._id) && (
+                    {item.starredBy.some((id) => id === currentUser?._id) && (
                       <Entypo name="star" size={13} />
-                    )}{" "}
-                    {format(new Date(parseInt(item.createdAt)), "p")}
+                    )}{' '}
+                    {format(new Date(parseInt(item.createdAt)), 'p')}
                   </Text>
                 </View>
               )}
@@ -188,60 +189,60 @@ const Message: React.FC<Props> = props => {
         ))}
       </ScrollView>
     </View>
-  );
-};
+  )
+}
 
-export default Message;
+export default Message
 
 const styles = StyleSheet.create({
   prt: {
     marginTop: 10,
-    marginBottom: 50
+    marginBottom: 50,
   },
   me: {
-    alignSelf: "flex-end",
-    backgroundColor: "#00af9c",
+    alignSelf: 'flex-end',
+    backgroundColor: '#00af9c',
     paddingHorizontal: 5,
-    maxWidth: "70%",
-    minWidth: "25%",
+    maxWidth: '70%',
+    minWidth: '25%',
     minHeight: 50,
     borderRadius: 5,
     paddingBottom: 20,
-    marginRight: 10
+    marginRight: 10,
   },
   sender: {
-    alignSelf: "flex-start",
-    backgroundColor: "#262d31",
+    alignSelf: 'flex-start',
+    backgroundColor: '#262d31',
     paddingHorizontal: 5,
-    maxWidth: "70%",
-    minWidth: "25%",
+    maxWidth: '70%',
+    minWidth: '25%',
     minHeight: 50,
     borderRadius: 5,
     paddingBottom: 20,
-    marginLeft: 10
+    marginLeft: 10,
   },
   meta: {
-    color: "rgba(255,255,255,.7)",
+    color: 'rgba(255,255,255,.7)',
     fontSize: 12,
-    position: "absolute",
+    position: 'absolute',
     bottom: 3,
     right: 5,
-    paddingLeft: 10
+    paddingLeft: 10,
   },
   selectedMsgPrt: {
     backgroundColor: AppColors.message_selection_highlight,
-    position: "absolute",
+    position: 'absolute',
     elevation: 1000,
     bottom: 0,
-    width: "100%"
+    width: '100%',
   },
   selectedMsg: {
     paddingHorizontal: 5,
-    maxWidth: "70%",
-    minWidth: "20%",
+    maxWidth: '70%',
+    minWidth: '20%',
     minHeight: 50,
     borderRadius: 5,
     paddingBottom: 20,
-    marginRight: 10
-  }
-});
+    marginRight: 10,
+  },
+})
