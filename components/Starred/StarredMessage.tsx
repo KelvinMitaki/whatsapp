651f24689b6efb1c2229964151e80cd40ad1cb3a
KelvinMitaki <mitakikelvin1@gmail.com>
2021-06-15 12:08:54 +0300

    wired up useFetchStarredMsgsQuery in StarredMessagesScreen

--- components/Starred/StarredMessage.tsx
+++ components/Starred/StarredMessage.tsx
@@ -1,22 +1,25 @@
-import React from "react";
-import { StyleSheet, Text, TouchableNativeFeedback, View } from "react-native";
-import { Ionicons, MaterialIcons, Entypo } from "@expo/vector-icons";
-import inspect from "../../inspect";
-import { NavigationInjectedProps, withNavigation } from "react-navigation";
-import { GroupMsg } from "../../interfaces/GroupInterface";
-import { MessageInterface } from "../../interfaces/ChatInterface";
-import { formatDate } from "../Home/ChatComponent";
-import { format } from "date-fns";
+import React from 'react';
+import { StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
+import { Ionicons, MaterialIcons, Entypo } from '@expo/vector-icons';
+import inspect from '../../inspect';
+import { NavigationInjectedProps, withNavigation } from 'react-navigation';
+import { GroupMsg } from '../../interfaces/GroupInterface';
+import { MessageInterface } from '../../interfaces/ChatInterface';
+import { formatDate } from '../Home/ChatComponent';
+import { format } from 'date-fns';
+import { FetchStarredMsgsQuery } from '../../generated/graphql';
 
 interface props {
-  starredMsg: MessageInterface | GroupMsg;
+  starredMsg:
+    | FetchStarredMsgsQuery['fetchStarredMsgs']['groupMsgs'][0]
+    | FetchStarredMsgsQuery['fetchStarredMsgs']['messages'][0];
 }
 
 const StarredMessage: React.FC<NavigationInjectedProps & props> = React.memo(
   ({ navigation, starredMsg }) => {
     return (
       <TouchableNativeFeedback
-        background={TouchableNativeFeedback.Ripple("#fff", false)}
+        background={TouchableNativeFeedback.Ripple('#fff', false)}
         onPress={() => {
           // navigation.navigate("Chat",{recipient})
         }}
@@ -25,10 +28,10 @@ const StarredMessage: React.FC<NavigationInjectedProps & props> = React.memo(
           <View style={styles.starredMsgHeader}>
             <View
               style={{
-                flexDirection: "row",
-                alignItems: "center",
+                flexDirection: 'row',
+                alignItems: 'center',
                 marginLeft: 10,
-                width: "75%"
+                width: '75%',
               }}
             >
               <View style={styles.person}>
@@ -37,20 +40,20 @@ const StarredMessage: React.FC<NavigationInjectedProps & props> = React.memo(
               <View
                 style={{
                   marginLeft: 10,
-                  flexDirection: "row",
-                  width: "80%",
-                  overflow: "hidden"
+                  flexDirection: 'row',
+                  width: '80%',
+                  overflow: 'hidden',
                 }}
               >
-                <Text style={{ color: "#fff" }} numberOfLines={1}>
+                <Text style={{ color: '#fff' }} numberOfLines={1}>
                   {
                     // @ts-ignore
                     starredMsg.sender.name
                   }
                 </Text>
                 <MaterialIcons name="arrow-right" size={20} color="#fff" />
-                <View style={{ maxWidth: "75%" }}>
-                  <Text style={{ color: "#fff" }} numberOfLines={1}>
+                <View style={{ maxWidth: '75%' }}>
+                  <Text style={{ color: '#fff' }} numberOfLines={1}>
                     {
                       // @ts-ignore
                       starredMsg.recipient ? starredMsg.recipient.name : starredMsg.group.name
@@ -59,19 +62,19 @@ const StarredMessage: React.FC<NavigationInjectedProps & props> = React.memo(
                 </View>
               </View>
             </View>
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <Text style={{ color: "rgba(255,255,255,.7)" }}>
-                {" "}
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text style={{ color: 'rgba(255,255,255,.7)' }}>
+                {' '}
                 {formatDate(new Date(parseInt(starredMsg.createdAt)))}
               </Text>
               <Entypo name="chevron-small-right" size={25} color="rgba(255,255,255,.7)" />
             </View>
           </View>
           <View style={styles.me}>
-            <Text style={{ color: "#fff" }}>{starredMsg.message}</Text>
+            <Text style={{ color: '#fff' }}>{starredMsg.message}</Text>
             <Text style={styles.meta}>
               <Entypo name="star" size={15} />
-              {format(new Date(parseInt(starredMsg.createdAt)), "p")}{" "}
+              {format(new Date(parseInt(starredMsg.createdAt)), 'p')}{' '}
               <Ionicons name="checkmark-done" size={18} />
             </Text>
           </View>
@@ -88,40 +91,40 @@ const styles = StyleSheet.create({
     height: 40,
     width: 40,
     borderRadius: 500,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "grey"
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'grey',
   },
   starredMsgPrt: {
     minHeight: 90,
     borderBottomWidth: 0.5,
-    borderBottomColor: "rgba(255,255,255,.4)"
+    borderBottomColor: 'rgba(255,255,255,.4)',
   },
   starredMsgHeader: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     height: 50,
-    alignItems: "center"
+    alignItems: 'center',
   },
   me: {
-    marginLeft: "15%",
-    backgroundColor: "#00af9c",
+    marginLeft: '15%',
+    backgroundColor: '#00af9c',
     paddingHorizontal: 5,
-    maxWidth: "70%",
-    minWidth: "25%",
-    alignSelf: "flex-start",
+    maxWidth: '70%',
+    minWidth: '25%',
+    alignSelf: 'flex-start',
     minHeight: 50,
     borderRadius: 5,
     paddingBottom: 20,
     marginBottom: 10,
-    marginRight: 10
+    marginRight: 10,
   },
   meta: {
-    color: "rgba(255,255,255,.7)",
+    color: 'rgba(255,255,255,.7)',
     fontSize: 12,
-    position: "absolute",
+    position: 'absolute',
     bottom: 3,
     right: 5,
-    paddingLeft: 10
-  }
+    paddingLeft: 10,
+  },
 });
--- screens/StarredMessagesScreen.tsx
+++ screens/StarredMessagesScreen.tsx
@@ -1,42 +1,43 @@
-import { useQuery } from "@apollo/client";
-import React from "react";
-import { ActivityIndicator, Dimensions, FlatList, StyleSheet, Text, View } from "react-native";
-import { NavigationStackScreenComponent, useHeaderHeight } from "react-navigation-stack";
-import { useSelector } from "react-redux";
-import AppColors from "../Colors/color";
-import HomeHeaderRight from "../components/Home/HomeHeaderRight";
-import StarredMessage from "../components/Starred/StarredMessage";
-import { FETCH_STARRED_MESSAGES } from "../graphql/queries";
-import inspect from "../inspect";
-import { StarredMessagesInterface } from "../interfaces/ChatInterface";
-import { Redux } from "../interfaces/Redux";
+import { useQuery } from '@apollo/client';
+import React from 'react';
+import { ActivityIndicator, Dimensions, FlatList, StyleSheet, Text, View } from 'react-native';
+import { NavigationStackScreenComponent, useHeaderHeight } from 'react-navigation-stack';
+import { useSelector } from 'react-redux';
+import AppColors from '../Colors/color';
+import HomeHeaderRight from '../components/Home/HomeHeaderRight';
+import StarredMessage from '../components/Starred/StarredMessage';
+import { useFetchStarredMsgsQuery } from '../generated/graphql';
+import { FETCH_STARRED_MESSAGES } from '../graphql/queries';
+import inspect from '../inspect';
+import { StarredMessagesInterface } from '../interfaces/ChatInterface';
+import { Redux } from '../interfaces/Redux';
 
 const StarredMessagesScreen: NavigationStackScreenComponent = () => {
   const searchModal = useSelector((state: Redux) => state.chat.searchModal);
   const starredInput = useSelector((state: Redux) => state.chat.starredInput);
   const headerHeight = useHeaderHeight();
-  const { data, loading } = useQuery(FETCH_STARRED_MESSAGES);
-  let starredMessages: StarredMessagesInterface | null = null;
+  const { data, loading } = useFetchStarredMsgsQuery();
+  let starredMessages = null;
   if (data && data.fetchStarredMsgs) {
     starredMessages = data.fetchStarredMsgs;
   }
   const starredMsgs = starredMessages
     ? [...starredMessages.messages, ...starredMessages.groupMsgs]
         .sort((a, b) => parseInt(a.createdAt) - parseInt(b.createdAt))
-        .map(msg => ({ ...msg, id: msg._id }))
+        .map((msg) => ({ ...msg, id: msg._id }))
     : [];
-  const screen = Dimensions.get("screen");
+  const screen = Dimensions.get('screen');
   return (
     <View>
       {searchModal && <View style={{ height: headerHeight }}></View>}
       {!loading ? (
         <FlatList
-          data={starredMsgs.filter(msg =>
+          data={starredMsgs.filter((msg) =>
             starredInput.trim().length
               ? msg.message.trim().toLowerCase().includes(starredInput.trim().toLowerCase())
               : true
           )}
-          keyExtractor={m => m._id}
+          keyExtractor={(m) => m._id}
           renderItem={({ item }) => <StarredMessage starredMsg={item} key={item._id} />}
         />
       ) : (
@@ -44,16 +45,16 @@ const StarredMessagesScreen: NavigationStackScreenComponent = () => {
           style={{
             height: screen.height,
             width: screen.width,
-            alignItems: "center",
-            justifyContent: "center"
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
           <ActivityIndicator size="large" color="#00af9c" />
-          <Text style={{ color: "rgba(255,255,255,.8)" }}>Fetching Messages...</Text>
+          <Text style={{ color: 'rgba(255,255,255,.8)' }}>Fetching Messages...</Text>
         </View>
       )}
       {!starredMsgs.length && !loading && (
-        <View style={{ height: "100%", alignItems: "center", justifyContent: "center" }}>
+        <View style={{ height: '100%', alignItems: 'center', justifyContent: 'center' }}>
           <Text style={{ color: AppColors.dull_white }}>No starred messages.</Text>
         </View>
       )}
@@ -62,8 +63,8 @@ const StarredMessagesScreen: NavigationStackScreenComponent = () => {
 };
 
 StarredMessagesScreen.navigationOptions = {
-  headerTitle: "Starred messages",
-  headerRight: () => <HomeHeaderRight starredMsgsScreen />
+  headerTitle: 'Starred messages',
+  headerRight: () => <HomeHeaderRight starredMsgsScreen />,
 };
 
 export default StarredMessagesScreen;
